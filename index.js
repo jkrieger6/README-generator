674462e0703cd4a6af51ccf4ca140dc4c21c3345
jkrieger6 <jkrieger11@gmail.com>
2023-04-15 12:25:27 -0700

    added fs variable

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs'); 
 
 // TODO: Create an array of questions for user input
-inquirer
-  .prompt const questions = {
+ const questions = {
     title: "What is the Title of your application?",
     describe: "How would you describe the functionality of the application?",
     install: "What are the steps required to install your app?",
@@ -12,6 +12,10 @@ inquirer
     howTo: "If anyone wants to contribute, how can they reach you?",
     roadMap: "Do you have any plans for the future of this application?"
 };
+
+inquirer.prompt(questions)
+.then((response) =>
+response.confirm === response.data)
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
