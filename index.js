29d73c57944ab7ee48838f7ba298d39744a0c45e
jkrieger6 <jkrieger11@gmail.com>
2023-04-18 15:17:51 -0700

    added none to license list

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ const generateMarkdown = require('./utils/generateMarkdown');
       "GNU General Public License v2.0",
       "GNU Lesser General Public License v2.1",
       "Mozilla Public License 2.0",
-      "The Unlicense"
+      "The Unlicense",
+      "None"
     ]
   },
   {
